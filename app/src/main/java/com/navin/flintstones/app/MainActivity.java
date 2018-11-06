55062a2da95158c3a9f45d45d839ffafd4aad831
Alireza <aesshoferi@gmail.com>
2018-11-06 22:32:27 +0330

    Improved used CompositeDisposable for disposables

--- app/src/main/java/com/navin/flintstones/app/MainActivity.java
+++ app/src/main/java/com/navin/flintstones/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.navin.flintstones.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.EditText;
@@ -16,6 +17,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 
 public class MainActivity extends Activity {
 
@@ -29,6 +31,9 @@ public class MainActivity extends Activity {
     TextView recdMessage;
 
     private RxWebSocket websocket;
+
+    @NonNull
+    private CompositeDisposable mCompositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +56,7 @@ public class MainActivity extends Activity {
     }
 
     private void logEvents() {
-        websocket.eventStream()
+        mCompositeDisposable.add(websocket.eventStream()
                 .observeOn(AndroidSchedulers.mainThread())
                 .doOnNext(event -> {
                     if (event instanceof RxWebSocket.Open) {
@@ -76,7 +81,7 @@ public class MainActivity extends Activity {
                     }
                 })
                 .subscribe(event -> {
-                }, this::logError);
+                }, this::logError));
     }
 
     private void logNewLine() {
@@ -101,45 +106,54 @@ public class MainActivity extends Activity {
     @OnClick({R.id.connect})
     void onConnect() {
         openWebsocket();
-        websocket.connect()
+        mCompositeDisposable.add(websocket.connect()
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         event -> Log.d(MainActivity.class.getSimpleName(), event.toString()),
-                        this::logError);
+                        this::logError));
     }
 
     @OnClick({R.id.disconnect})
     void onDisconnect() {
         if (websocket != null) {
-            websocket.disconnect(1000, "Disconnect")
+            mCompositeDisposable.add(websocket.disconnect(1000, "Disconnect")
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(
                             event -> Log.d(MainActivity.class.getSimpleName(), event.toString()),
-                            this::logError);
+                            this::logError));
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (!mCompositeDisposable.isDisposed()) {
+            mCompositeDisposable.dispose();
+        }
+
+        super.onDestroy();
     }
 
     @OnClick({R.id.send})
     void onSend() {
         if (websocket != null) {
-            websocket
+            mCompositeDisposable.add(websocket
                     .send(sendMessage.getText().toString())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(
                             event -> Log.d(MainActivity.class.getSimpleName(), event.toString()),
-                            this::logError);
+                            this::logError));
         }
     }
 
     @OnClick({R.id.send_sample_obj})
     void onSendObject() {
         if (websocket != null) {
-            websocket
+            mCompositeDisposable.add(websocket
                     .send(new SampleDataModel(1, "sample object"))
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(
                             event -> Log.d(MainActivity.class.getSimpleName(), event.toString()),
-                            this::logError);
+                            this::logError));
         }
     }
 
