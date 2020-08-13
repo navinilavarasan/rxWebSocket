12b2298e23fd4edd2b0010d1b89fd025b41c0f35
Navin Ilavarasan <navin.ilavarasan@gmail.com>
2020-08-13 17:49:41 +0530

    Update WebSocketInterceptor.java

--- rxwebsocket/src/main/java/com/navin/flintstones/rxwebsocket/WebSocketInterceptor.java
+++ rxwebsocket/src/main/java/com/navin/flintstones/rxwebsocket/WebSocketInterceptor.java
@@ -1,10 +1,3 @@
-/*
- *  Copyright (C) Allegion - All Rights Reserved
- *  Unauthorized copying of this file, via any medium is strictly prohibited
- *  Proprietary and confidential
- *
- */
-
 package com.navin.flintstones.rxwebsocket;
 
 public interface WebSocketInterceptor {
